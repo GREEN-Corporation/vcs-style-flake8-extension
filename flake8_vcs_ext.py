b4cff7ab94ab47d87065363f7305f5bcc3a85f9c
Debianov <regdeb@mail.ru>
2023-06-29 22:02:55 +0800

    Добавлен код для попытки запустить расширение на flake8

--- flake8_vcs_ext.py
+++ flake8_vcs_ext.py
@@ -1,62 +1,30 @@
-import ast
-from typing import Generator, Tuple, Type, Any, List, Union, Final
+import tokenize
+from typing import Generator, Tuple, Type, Any, List, Union, Final, Optional
 
 import importlib.metadata as importlib_metadata
 
 MSG_VCS001: Final = "VCS001 no tab for line continuation"
-MSG_VCS002: Final = "VCS002 no exactly 1 blank line after the last import at the beginning of the file"
 
-BLOCKS_REQUIRING_INDENT = (
-	ast.Import,
-	ast.ImportFrom
-)
+# class Visitor(ast.NodeVisitor):
 
-ModuleLikeTypes = Union[
-   ast.Import,
-	ast.ImportFrom
-]
+# 	def __init__(self) -> None:
+# 		self.problems: List[Tuple[int, int]] = []
 
-class Visitor(ast.NodeVisitor):
+# 	def generic_visit(self, node: ModuleLikeTypes) -> None:
+# 		self._check_one_node(node)
 
-	def __init__(self) -> None:
-		self.problems: List[Tuple[int, int]] = []
+# 	def _check_one_node(self, node: ModuleLikeTypes) -> None:
+# 		if 'body' in node.__dict__:
+# 			self._check_a_list_of_items(node.body)
 
-	def generic_visit(self, node: ModuleLikeTypes) -> None:
-		self._check_one_node(node)
-
-	def _check_one_node(self, node: ModuleLikeTypes) -> None:
-		if 'body' in node.__dict__:
-			self._check_a_list_of_items(node.body)
-
-		if 'orelse' in node.__dict__:
-			self._check_a_list_of_items(node.orelse)
-
-		if 'handlers' in node.__dict__:
-			self._check_a_list_of_items(node.handlers)
-
-		if 'finalbody' in node.__dict__:
-			self._check_a_list_of_items(node.finalbody)
-
-	def _check_a_list_of_items(self, item_list) -> None:
-		for i, this_item in enumerate(item_list):
-			next_item = item_list[i + 1]
-			if (
-				not isinstance(next_item, ast.ExceptHandler)
-				and next_item.lineno - this_item.end_lineno <= 1
-			):
-				line = this_item.end_lineno
-				col = 0
-				self.problems.append((line, col))
+# 	def _check_a_list_of_items(self, item_list: ModuleLikeTypes) -> None:
+# 		pass
 
 class Plugin:
-	name = __name__
-	version = importlib_metadata.version("vcs-style-flake8-extension")
 
-	def __init__(self, tree: ast.AST) -> None:
-		self._tree = tree
+	def __init__(self, blank_before, blank_lines, checker_state, indent_char, indent_level) -> None:
+		print(blank_before, blank_lines, indent_char, indent_level)
 
 	def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
-		visitor = Visitor()
-		visitor.visit(self._tree)
-		for line, col in visitor.problems:
-			yield line, col, MSG_VCS001, type(self)
+		raise TestError
+		yield 21, 43, MSG_VCS001, type(self)
