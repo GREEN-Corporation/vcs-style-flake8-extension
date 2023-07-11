55b229fd8c4654b33241b2f3c7a90faa940d5c6e
Debianov <regdeb@mail.ru>
2023-07-11 22:15:45 +0800

    Изменён неверно сформированный случай (ошибка flake8 выходит и без расширения).

--- tests/bad_cases.py
+++ tests/bad_cases.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Literal
+from typing import Tuple, Literal
 
 bad_case = Tuple[Literal[''], int, int]
 
@@ -9,13 +9,13 @@ case_two_tabs = (
 
 case_two_tabs = (case_two_tabs, 2, 2)
 
-case_if_four_tabs_and_space = ( 
+case_if_with_diff_indents = (
 """if (aboba
-				 and string):
+				and string):
 					pass"""
 )
 
-case_if_four_tabs_and_space = (case_if_four_tabs_and_space, 2, 4)
+case_if_with_diff_indents = (case_if_with_diff_indents, 2, 4)
 
 case_def_with_diff_indents = (
 """def base(
@@ -44,7 +44,7 @@ case_in_context = (case_in_context, 6, 2)
 def collect_all_cases() -> Tuple[bad_case]:
 	return (
 		case_two_tabs, # type: ignore
-		case_if_four_tabs_and_space,
+		case_if_with_diff_indents,
 		case_def_with_diff_indents,
 		case_in_context
 	)
